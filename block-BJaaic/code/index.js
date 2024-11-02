1eb04aed18f650937e766a7289cdbd1e711dae0d
Piyush Chamoli <piyushchamoli11@gmail.com>
2024-11-02 16:15:25 +0530

    add change

--- block-BJaaic/code/index.js
+++ block-BJaaic/code/index.js
@@ -1,4 +1,4 @@
-
+/* 
 let animalMethods = {
     eat: function () {
         return `I live in ${this.location} and I can eat`
@@ -66,4 +66,75 @@ function cat (location,legs,name,color) {
     newCat.name = name;
     newCat.colorOfEyes = color;
     return newCat;
+}
+
+*/
+
+let animalMethods = {
+  eat() {
+    return `I live in ${this.location} and I can eat`;
+  },
+  changeLocation(newLocation) {
+    this.location = newLocation;
+  },
+  summary() {
+    return `I live in ${this.location} and I have ${this.numberOfLegs}`;
+  },
+};
+
+let dogMethods = {
+  bark() {
+    alert(`I am ${this.name} and I can bark 🐶`);
+  },
+  changeName(newName) {
+    this.name = newName;
+  },
+  changeColor(newColor) {
+    this.color = newColor;
+  },
+  summary() {
+    return `I am ${this.name} and I am of ${this.color} color. I can also bark`;
+  },
+};
+
+Object.setPrototypeOf(dogMethods, animalMethods);
+
+let catMethods = {
+  meow: function () {
+    alert(`I am ${this.name} and I can do meow meow 😹`);
+  },
+  changeName: function (newName) {
+    this.name = newName;
+  },
+  changeColorOfEyes: function (newColor) {
+    this.colorOfEyes = newColor;
+  },
+  summary: function () {
+    return `I am ${this.name} and the color of my eyes are ${this.colorOfEyes}. I can also do meow meow`;
+  },
+};
+
+Object.setPrototypeOf(catMethods, animalMethods);
+
+function animal(loc, legs) {
+  let animal = Object.create(animalMethods);
+  animal.location = loc;
+  animal.numberOfLegs = legs;
+  return animal;
+}
+
+function dog(loc, legs, name, color) {
+  let newDog = animal(loc, legs);
+  Object.setPrototypeOf(newDog, dogMethods);
+  newDog.name = name;
+  newDog.color = color;
+  return newDog;
+}
+
+function cat(location, legs, name, color) {
+  let newCat = animal(location, legs);
+  Object.setPrototypeOf(newCat, catMethods);
+  newCat.name = name;
+  newCat.colorOfEyes = color;
+  return newCat;
 }
